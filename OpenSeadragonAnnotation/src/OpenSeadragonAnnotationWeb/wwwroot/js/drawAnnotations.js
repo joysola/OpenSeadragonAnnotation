c42b3df63193da6cf8285dbe26bf72773c9d92ec
joysola <chenwenjun@deepsight.top>
2020-12-15 18:47:59 +0800

    1. openseadragon层叠选中判读。

--- OpenSeadragonAnnotation/src/OpenSeadragonAnnotationWeb/wwwroot/js/drawAnnotations.js
+++ OpenSeadragonAnnotation/src/OpenSeadragonAnnotationWeb/wwwroot/js/drawAnnotations.js
@@ -1,8 +1,14 @@
 ﻿
 
-var self = this;
+
 var isDraw = false;
-var olay = {};
+var isFreeDraw = false;
+var olay = {}; // openseadragon的FabricjsOverlay用于使用fabricjs
+var view = {};
+var strokeWidth = 0.5;
+var borderColor = 'black';
+var cornerColor = 'black';
+var cornerSize = 12;
 var url = 'http://localhost:5001/Annotation'
 window.onload = function () {
     var viewer = OpenSeadragon({
@@ -24,67 +30,65 @@ window.onload = function () {
             }
         }
     });
+    view = viewer;
     // 初始化标记功能
     InitFabricOverlay(viewer);
-    // 画标注按钮
-    document.getElementById("rect").onclick = event => {
+    // 绘制标注的按钮功能
+    $("#rect").click(event => {
         isDraw = !isDraw;
         viewer.setMouseNavEnabled(!isDraw);
         viewer.outerTracker.setTracking(!isDraw);
-    };
+        let canvas = olay.fabricCanvas();
+        // 修改按钮显示
+        if (isDraw) {
+            // 绘制时所有对象都不可以选中
+            canvas.getObjects().forEach(o => o.set('selectable', false));
+            $('#rect').text("结束绘制");
+        } else {
+            // 不绘制时所有对象皆可选中
+            canvas.getObjects().forEach(o => o.set('selectable', true));
+            $('#rect').text("开始绘制");
+        }
+    });
+    $("#draw").click(event => {
+        isFreeDraw = !isFreeDraw;
+        //viewer.setMouseNavEnabled(!isDraw);
+        //viewer.outerTracker.setTracking(!isDraw);
+        // 修改按钮显示
+        if (isFreeDraw) {
+            btnDraw(true, olay, null, null);
+            $('#draw').text("绘制结束");
+        } else {
+            btnDraw(false, olay, null, null);
+            $('#draw').text("自由绘制");
+        }
+    });
     // 获取标注
     $.get(`${url}/GetAnnoMarks`, data => {
         data.forEach(x => {
             if (x.type === "rect") {
                 var r = new fabric.Rect(x);
+                r.set({
+                    borderColor: borderColor,
+                    cornerColor: cornerColor,
+                    cornerSize: cornerSize,
+                    transparentCorners: true
+                    //borderScaleFactor: 5
+                });
                 olay.fabricCanvas().add(r);
             }
         });
     });
-    //初始化右击菜单(用于删除)
-    $.contextMenu({
-        selector: '#openSeadragon1',
-        trigger: 'right',
-        //构建菜单项build方法在每次右键点击会执行
-        build: function ($trigger, e) {
-            let canvas = olay.fabricCanvas();
-            let anno = canvas.getActiveObject();
-            // 选中，且鼠标在选中的标注中，显示删除
-            if (anno && canvas.containsPoint(event, anno)) {
-                return {
-                    callback: (key, options) => {
-                        if (key === 'delete') {
-                            let canvas = olay.fabricCanvas();
-                            let anno = canvas.getActiveObject(); // 被选中的anno
-                            canvas.remove(anno); // 删除
-                            canvas.requestRenderAll(); // 重新生成画布
-                            let guid = anno.guid;
-                            let id = anno.id;
-                            $.ajax({
-                                type: "post",
-                                url: `${url}/DeleteAnno?guid=${guid}&id=${id}`,
-                                contentType: 'application/json',
-                                data: JSON.stringify(anno),
-                                success: function (data, status) {
-                                    if (status === "success") {
-                                        console.log("删除标注成功！");
-                                    }
-                                }
-                            });
-                        }
-                    },
-                    items: {
-                        "delete": { name: "删除", icon: "delete" }
-                    }
-                };
-            }
-
-        }
-
-    });
+    // 初始化右键菜单
+    InitContextMenu(olay);
 };
-
+// 初始化Fabric用于构建标注
 function InitFabricOverlay(viewer) {
+    // 初始化选中的边框颜色
+    //fabric.Object.prototype.transparentCorners = false;
+    //fabric.Object.prototype.cornerStrokeColor = 'red';
+    //fabric.Object.prototype.strokeWidth = 4;
+    fabric.Object.prototype.strokeUniform = true; // 边框和放大无关，恒定大小
     var isDown;
     // Initialize overlay
     var options = {
@@ -92,58 +96,39 @@ function InitFabricOverlay(viewer) {
     }
     var overlay = viewer.fabricjsOverlay(options);
     olay = overlay;
+    // 自由画刷设置
+    overlay.fabricCanvas().freeDrawingBrush.color = 'red';
+    overlay.fabricCanvas().freeDrawingBrush.width = 10;
     // Add fabric rectangle
     var rect = {};
-    //overlay.fabricCanvas().fireRightClick = true; // 允许右击
-    //var rect2 = new fabric.Rect({
-    //    left: 666,
-    //    top: 666,
-    //    //fill: 'red',
-    //    fill: '',
-    //    width: 200,
-    //    height: 200,
-    //    stroke: 'red', // 边框颜色
-    //    strokeWidth: 3 // 线宽
-    //});
 
-    //overlay.fabricCanvas().add(rect2);
-    var text = new fabric.Text('Start\n drawing', {
-        left: 950,
-        top: 150,
-        fontSize: 50,
-        fontFamily: 'Helvetica',
-        textAlign: 'center',
-        fill: 'black',
-        selectable: false,
-        action: 'button'
 
-    });
-    overlay.fabricCanvas().add(text);
     // Add fabric circle with text
-    var circle2 = new fabric.Circle({
-        left: 930,
-        top: 100,
-        fill: 'lightgray',
-        radius: 120,
-        selectable: false,
-        action: 'button'
-    });
-    overlay.fabricCanvas().add(circle2);
+    //var circle2 = new fabric.Circle({
+    //    left: 930,
+    //    top: 100,
+    //    fill: 'lightgray',
+    //    radius: 120,
+    //    selectable: false,
+    //    action: 'button'
+    //});
+    //overlay.fabricCanvas().add(circle2);
+    // 不可编辑文本框
+    //var txtbox = new fabric.Textbox('你好', {
+    //    left: 254,
+    //    top: 195,
+    //    width: 300,
+    //    height: 75.03,
+    //    fill: "#ecb2cc",
+    //    angle: 0,
 
-    var txtbox = new fabric.Textbox('你好', {
-        left: 254,
-        top: 195,
-        width: 300,
-        height: 75.03,
-        fill: "#ecb2cc",
-        angle: 0,
+    //    fontSize: 20,
 
-        fontSize: 20,
+    //    fontFamily: "helvetica"
+    //});
+    //overlay.fabricCanvas().add(txtbox);
 
-        fontFamily: "helvetica"
-    });
-    overlay.fabricCanvas().add(txtbox);
-
+    // 可编辑文本框
     var itxt = new fabric.IText('你好\n迪赛特', {
         left: 300,
         top: 400,
@@ -165,10 +150,6 @@ function InitFabricOverlay(viewer) {
         if (window.isDraw) {
             isDown = false;
             try {
-                var tmp = JSON.parse(JSON.stringify(rect))
-                //$.post("http://localhost:5001/Annotation/StoreAnno", tmp, function (data) {
-                //    rect = {};
-                //});
                 $.ajax({
                     type: "post",
                     url: `${url}/StoreAnno`,
@@ -189,9 +170,9 @@ function InitFabricOverlay(viewer) {
         }
         if (isRealValue(event.target)) {
 
-            if (event.target.type === "path") {
-                btnDraw(false, overlay, circle2, text);
-            }
+            //if (event.target.type === "path") {
+            //    btnDraw(false, overlay, circle2, text);
+            //}
 
             // Animate circle on mouse release (try to drag circle up)
             //if (event.target.action === 'gravity') {
@@ -211,7 +192,7 @@ function InitFabricOverlay(viewer) {
             //}
 
         } else {
-            btnDraw(false, overlay, circle2, text);
+            //btnDraw(false, overlay, circle2, text);
         }
 
     })
@@ -228,12 +209,12 @@ function InitFabricOverlay(viewer) {
         console.log(event);
     });
     overlay.fabricCanvas().on('mouse:over', event => {
-        console.log('mouse: over');
-        console.log(event);
+        //console.log('mouse: over');
+        //console.log(event);
     });
-    //拖动后产生事件
+    //变化后产生事件
     overlay.fabricCanvas().on('object:modified', event => {
-        overlay.fabricCanvas().requestRenderAll(); // 重新生成画布,防止数据未更新
+        overlay.fabricCanvas().renderAll(); // 重新生成画布,防止数据未更新
         console.log('object:modified');
         console.log(event);
         if (event.target.type === 'rect') {
@@ -254,19 +235,33 @@ function InitFabricOverlay(viewer) {
     });
     overlay.fabricCanvas().on('mouse:out', event => {
         if (isRealValue(event.target) && event.target.canvas.getActiveObject() && event.target.canvas.getActiveObject().type === 'i-text') {
-
             var tag = event.target;
             tag.canvas.setActiveObject(tag.canvas.getActiveObject());
             tag.canvas.getActiveObject().editable = false;
             tag.canvas.getActiveObject().exitEditing();
         }
     });
-
-    overlay.fabricCanvas().freeDrawingBrush.color = 'red';
-    overlay.fabricCanvas().freeDrawingBrush.width = 30;
-
-    // Start free-drawing mode
-    overlay.fabricCanvas().on('mouse:down', (event) => {
+    var selectedObj = {};
+    overlay.fabricCanvas().on('selection:updated', event => {
+        selectedObj = event.target;
+    });
+    //overlay.fabricCanvas().on('mouse:down:before', event => {
+    //    if (!isRealValue(event.target)) {
+    //        return;
+    //    }
+    //    let res = checkOverlap(overlay); // 层叠处理
+    //    if (selectedObj.guid != event.target.guid) {
+    //        event.target = res;
+    //        let xx = JSON.parse(JSON.stringify(event))
+    //        event.e.preventDefaultAction = false;
+    //        overlay.fabricCanvas().fire("mouse:down",xx);
+    //    } else {
+    //        //event.e.preventDefaultAction = true;
+    //        console.log(event.target.guid);
+    //    }
+    //});
+    overlay.fabricCanvas().on('mouse:down', event => {
+        // 画标注
         if (window.isDraw) {
             isDown = true;
             var pointer = overlay.fabricCanvas().getPointer(event.e);
@@ -283,22 +278,30 @@ function InitFabricOverlay(viewer) {
                 angle: 0,
                 fill: '',
                 stroke: 'red',
-                strokeWidth: 3,
-                transparentCorners: true
+                strokeWidth: strokeWidth,
+                borderColor: borderColor,
+                cornerColor: cornerColor,
+                cornerSize: cornerSize,
+                // transparentCorners: false // 是否是透明选中框
             });
             overlay.fabricCanvas().add(rect);
             return;
+        } else { // 选中判读
+            //if (selectedObj.guid != event.target.guid) {
+            checkOverlap(overlay); // 层叠处理
+            //}
+            //if (event.button === 3) {
+            //viewer.setMouseNavEnabled(false);
+            //viewer.outerTracker.setTracking(false);
+            // }
+            //if (event.target.action === 'button') {
+            //    btnDraw(true, overlay, circle2, text);
+            //}
+
+            //event.preventDefault();
         }
 
-        if (isRealValue(event.target)) {
-            if (event.button === 3) {
-                //viewer.setMouseNavEnabled(false);
-                //viewer.outerTracker.setTracking(false);
-            }
-            if (event.target.action === 'button') {
-                btnDraw(true, overlay, circle2, text);
-            }
-        }
+
 
     });
     overlay.fabricCanvas().on('mouse:move', function (o) {
@@ -323,38 +326,123 @@ function InitFabricOverlay(viewer) {
     function isRealValue(obj) {
         return obj && obj !== 'null' && obj !== 'undefined';
     }
-    /**
-      * Toggle button on and off
-      */
-    function btnDraw(flag, over, shape, txt) {
-        // Toggle fabric canvas draw
-        if (flag) {
-            // Disable OSD mousevents
-            viewer.setMouseNavEnabled(false);
-            viewer.outerTracker.setTracking(false);
-            // Activate fabric freedrawing mode
-            over.fabricCanvas().isDrawingMode = true;
-            // Button
-            shape.set('fill', 'lightgreen');
-            txt.text = 'Draw';
-            txt.left = 990;
-            over.fabricCanvas().add(txt);
-        } else {
-            // Stop drawing & switch back to zoom
-            // Add tracking back to OSD
-            viewer.setMouseNavEnabled(true);
-            viewer.outerTracker.setTracking(true);
-            // Disable freedrawing mode
-            over.fabricCanvas().isDrawingMode = false;
-            // Button
-            shape.set('fill', 'lightgray');
-            over.fabricCanvas().add(shape);
-            txt.text = 'Start\n drawing';
-            txt.left = 950;
-            over.fabricCanvas().add(txt);
+    // 检查重叠
+    function checkOverlap(overlay) {
+        let canvas = overlay.fabricCanvas();
+        let allObjs = canvas.getObjects(); // 画布上所有的对象
+        let tmp = []; // 鼠标所在位置所有的对象
+        allObjs.forEach(o => {
+            if (canvas.containsPoint(event, o)) {
+                tmp.push(o);
+            }
+        });
+        let map = new Map(); // 记录包含了对象的 对象
+        for (let i = 0; i < tmp.length; i++) {
+            for (let j = i + 1; j < tmp.length; j++) {
+                let objA = tmp[i];
+                let objB = tmp[j];
+                // B 包含 A
+                if (objA.isContainedWithinObject(objB)) {
+                    if (!map.has(objB.guid)) {
+                        map.set(objB.guid, objB);
+                    }
+                    continue;
+                }
+                // A 包含 B
+                if (objB.isContainedWithinObject(objA)) {
+                    if (!map.has(objA.guid)) {
+                        map.set(objA.guid, objA);
+                    }
+                    continue;
+                }
+            }
+        }
+        let result = tmp.filter(t => !map.has(t.guid)); // 去掉包含了对象的对象
+        if (result.length > 0) {
+            canvas.setActiveObject(result[0]); // 设定选中项
+            console.log(`选中项${result[0].guid}`);
+            canvas.renderAll(); // 重新生成画布，防止不能及时更新选中项
+            return result[0];
         }
     }
-    function registerMouseEvent(overlay) {
 
+}
+// 初始化右键菜单用于删除
+function InitContextMenu(olay) {
+    //初始化右击菜单(用于删除)
+    $.contextMenu({
+        selector: '#openSeadragon1',
+        trigger: 'right',
+        //构建菜单项build方法在每次右键点击会执行
+        build: function ($trigger, e) {
+            let canvas = olay.fabricCanvas();
+            let anno = canvas.getActiveObject(); // 被选中的anno
+            // 选中，且鼠标在选中的标注中，显示删除
+            if (anno && canvas.containsPoint(event, anno)) {
+                return {
+                    callback: (key, options) => {
+                        if (key === 'delete') {
+                            // let canvas = olay.fabricCanvas();
+                            // let anno = canvas.getActiveObject(); // 被选中的anno
+                            canvas.remove(anno); // 删除
+                            canvas.requestRenderAll(); // 重新生成画布
+                            let guid = anno.guid;
+                            let id = anno.id;
+                            try {
+                                $.ajax({
+                                    type: "post",
+                                    url: `${url}/DeleteAnno?guid=${guid}&id=${id}`,
+                                    contentType: 'application/json',
+                                    data: JSON.stringify(anno),
+                                    success: function (data, status) {
+                                        if (status === "success") {
+                                            console.log("删除标注成功！");
+                                        }
+                                    }
+                                });
+                            } catch (e) {
+                                console.log("删除标注失败！");
+                            }
+
+                        }
+                    },
+                    items: {
+                        "delete": { name: "删除", icon: "delete" }
+                    }
+                };
+            }
+
+        }
+
+    });
+}
+// 自由绘制
+function btnDraw(flag, over, shape, txt) {
+    // Toggle fabric canvas draw
+    //if (flag) {
+    if (window.isFreeDraw) {
+        // Disable OSD mousevents
+        view.setMouseNavEnabled(false);
+        view.outerTracker.setTracking(false);
+        // Activate fabric freedrawing mode
+        over.fabricCanvas().isDrawingMode = true;
+        // Button
+        // shape.set('fill', 'lightgreen');
+        // txt.text = 'Draw';
+        // txt.left = 990;
+        // over.fabricCanvas().add(txt);
+    } else {
+        // Stop drawing & switch back to zoom
+        // Add tracking back to OSD
+        view.setMouseNavEnabled(true);
+        view.outerTracker.setTracking(true);
+        // Disable freedrawing mode
+        over.fabricCanvas().isDrawingMode = false;
+        // Button
+        // shape.set('fill', 'lightgray');
+        // over.fabricCanvas().add(shape);
+        //  txt.text = 'Start\n drawing';
+        // txt.left = 950;
+        // over.fabricCanvas().add(txt);
     }
 }
